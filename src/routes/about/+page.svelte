84867b50d4d7fed8713f6f097893a20fd4c7e13a
prashantkon <prashant777k@gmail.com>
2023-03-26 13:44:20 -0700

    Used for the about me section.

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+    import duck from '$lib/images/duck.jpg';
+</script>
+
+<head>
+	<title>About Me</title>
+</head>
+
+<div class="container">
+    
+    <!-- svelte-ignore missing-declaration -->
+    <img alt="The project logo" src={duck} />
+    <h1>Our Team</h1>
+    <p>Hello, we are Daniel Cui, Bhargav Eranki, Prashant Kondayapalepu, Shine Chang. </p>
+    <p>Feel free to get in touch with me at [insert email or social media links here].</p>
+</div>
+
+
+
+
+<style>
+    body {
+        font-family: Arial, sans-serif;
+        background-color: rgb(15, 30, 231);
+    }
+
+    h1 {
+        color: #333333;
+        text-align: center;
+    }
+
+    .container {
+        max-width: 600px;
+        margin: 0 auto;
+        padding: 20px;
+        background-color: #ffffff;
+        border-radius: 10px;
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    }
+
+    img {
+        display: block;
+        margin: 0 auto;
+        width: 200px;
+        border-radius: 50%;
+    }
+
+    p {
+        margin-top: 20px;
+        line-height: 1.5;
+        text-align: justify;
+    }
+</style>
