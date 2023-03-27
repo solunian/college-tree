1f56f2b98e06521a28afb432b075ae69d216aa52
Shine Chang <shine00chang@gmail.com>
2023-03-26 18:25:34 -0700

    feed load

--- src/routes/feed/+page.svelte
+++ src/routes/feed/+page.svelte
@@ -1,11 +1,26 @@
 <script>
     import Post from "$lib/components/Post.svelte";
+    const path = "http://localhost:2023";
+
+    let username = "";
+    let feed = [];
+    const handleSubmitUsername = async () => {
+		const res = await fetch(path + `/posts/${username}`);
+		feed = await res.json();
+    };
 </script>
 
 <div>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
+	{#each feed as post}
+        <h>{post.title}</h>
+        <Post />
+    {:else}
+        <h2>Enter username:</h2>
+        <form on:submit|preventDefault={handleSubmitUsername} class="flex flex-col gap">
+            <input type="text" name="username" bind:value={username}/>
+            <button type=submit>
+                submit
+            </button>
+        </form>
+    {/each}
 </div>
