163db3d9da488f334e706ea0455d3c3383aa3b91
Daniel Cui <solunian@gmail.com>
2023-03-26 18:13:44 -0700

    work on home page

--- src/lib/components/CreateButton.svelte
+++ src/lib/components/CreateButton.svelte
@@ -9,7 +9,7 @@
 
 
 <button class="fixed bottom-5 right-5 w-12 h-12 text-5xl rounded-2xl outline outline-1 outline-gray-300 
-p-2 hover:bg-purple-200 duration-200 ease-in-out" 
+p-2 bg-gray-300 hover:bg-purple-200 hover:outline-purple-300 duration-300 ease-in-out" 
 on:click={createPostClickHandler}>
     <img src={plusIcon} alt=""/>
 </button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,20 @@
 <script>
-    import Graduation from '$lib/images/Grad.jpg';
+    import Graduation from '$lib/images/Grad.png';
 </script>
 
 
+<div>
+    <img alt="The project logo" src={Graduation} class="w-full p-0 -z-20" />
 
-<head>
-	<title>Welcome to College Tree!</title>
-</head>
-<style>
-    .container {
-      text-align: center;
-    }
-  </style>
+    <div class="px-36">
+        <div class="flex flex-col">
+            <h1 class="text-3xl">
+              branch out towards college!
+            </h1>
+            <h5>Mission Statement</h5>
+            <p>Our mission is to connect high school students with oppurtunities related to their career choices. Many high school students lack the oppurtunity to participate in activites because they simply can't find anything that fits their future career. College Tree targets these individuals and helps them by connecting them to events in relation to their interests. We acheived this by using a database with stores information of the user and searches based on their characteristics.    </p>
+            <p>By connecting high school students with topics that they are interested in, we believe they have a better chance in getting into the college of their choice. </p>
+        </div>
 
-<div class="container">
-    
-    <!-- svelte-ignore missing-declaration -->
-    <img alt="The project logo" src={Graduation} class="w-full" />
-    <h1><b>Branch out towards College </b></h1>
-    <h5>Mission Statement</h5>
-    <p>Our mission is to connect high school students with oppurtunities related to their career choices. Many high school students lack the oppurtunity to participate in activites because they simply can't find anything that fits their future career. College Tree targets these individuals and helps them by connecting them to events in relation to their interests. We acheived this by using a database with stores information of the user and searches based on their characteristics.    </p>
-    <p>By connecting high school students with topics that they are interested in, we believe they have a better chance in getting into the college of their choice. </p>
+    </div>
 </div>
