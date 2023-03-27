7773d0b059e484b8826baf8fd6dec859c6fd6bef
Daniel Cui <solunian@gmail.com>
2023-03-26 18:29:41 -0700

    resolve merge conflict?

--- src/lib/components/Post.svelte
+++ src/lib/components/Post.svelte
@@ -1,13 +1,17 @@
 <script>
     import Tag from "$lib/components/Tag.svelte";
     import placeholderImg from "$lib/images/placeholder.gif";
+
+    let title = "", author = "", text = "", major = "";
+    
 </script>
 
 <div class="flex flex-col py-2 px-2 border rounded-xl m-5 bg-slate-50">
-    <h2>Post Title</h2>
+    <h2>{title}</h2>
+    <h3>{author}</h3>
     <img src={placeholderImg} alt="" class="w-96 rounded-md"/>
-    
-    
+    <p>{text}</p>
+    <p>{major}</p>
     <div class="flex flex-row gap-1 flex-wrap">
         <Tag/><Tag/>
         <Tag/><Tag/>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,13 +6,13 @@
 <div>
     <img alt="The project logo" src={Graduation} class="w-full p-0 -z-20" />
 
-    <div class="px-36">
-        <div class="flex flex-col">
-            <h1 class="text-3xl">
-              branch out towards college!
+    <div class="px-56 pt-5">
+        <div class="flex flex-col gap-4">
+            <h1 class="text-3xl my-5">
+              Branch out with <b class="text-purple-900">college tree</b>!
             </h1>
-            <h5>Mission Statement</h5>
-            <p>Our mission is to connect high school students with oppurtunities related to their career choices. Many high school students lack the oppurtunity to participate in activites because they simply can't find anything that fits their future career. College Tree targets these individuals and helps them by connecting them to events in relation to their interests. We acheived this by using a database with stores information of the user and searches based on their characteristics.    </p>
+            <h2 class="text-2xl">Mission Statement</h2>
+            <p>Our mission is to connect high school students with oppurtunities related to their career choices. Many high school students lack the oppurtunity to participate in activites because they simply can't find anything that fits their future career. College Tree targets these individuals and helps them by connecting them to events in relation to their interests. We acheived this by using a database with stores information of the user and searches based on their characteristics.</p>
             <p>By connecting high school students with topics that they are interested in, we believe they have a better chance in getting into the college of their choice. </p>
         </div>
 
