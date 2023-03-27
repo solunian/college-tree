8a32d5c06637207680c86539cef65c969e60786e
Shine Chang <shine00chang@gmail.com>
2023-03-26 18:03:05 -0700

    profile integration

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,17 +1,47 @@
 <script>
+    const path = "http://localhost:2023";
     let editing = false; // is editing the profile
 
-    let username = "";
+    let username = "", bio = "", majors = "", tags = "";
     const handleSubmitUsername = () => {
-        // fetch();
-        editing = true;
+        fetch(path + `/user/${username}`)
+        .then(res => res.json())
+        .then(res => {
+            console.log("res from submit username: ", res);
+            if (res.err) {
+                console.log("none such user.");
+                return;
+            }
+
+            username = res.name;
+            bio = res.bio;
+            majors = res.majors;
+            tags = res.tags;
+            
+            console.log(res);
+            editing = true;
+        });
     };
 
-    let bio = "", major = "", tags = "";
     const handleSaveChanges = () => {
-        // fetch(); put?
-        // goto();
-        editing = false;
+        fetch(path + `/user/edit/${username}`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            body: JSON.stringify({
+                name : username,
+                bio : bio,
+                majors : majors,
+                tags  : tags,
+            })
+        })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            editing = false;
+        });
     };
 </script>
 
@@ -28,7 +58,7 @@
     </h2>
     <form on:submit|preventDefault={handleSaveChanges} class="flex flex-col gap">
         <label for="bio">bio</label><input type="text" name="bio" bind:value={bio}/>
-        <label for="major">major</label><input type="text" name="major" bind:value={major}/>
+        <label for="majors">major</label><input type="text" name="majors" bind:value={majors}/>
         <label for="tags">tags</label><input type="text" name="tags" bind:value={tags}/>
 
         <!-- author, title, text, attachments, major, tags -->
